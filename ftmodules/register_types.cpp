e7da6cdf9c90b891dfa71aacee116268206ba1c8
Empika1 <dravidts1@gmail.com>
2025-07-07 23:32:52 -0400

    make it build and add the game

--- ftmodules/register_types.cpp
+++ ftmodules/register_types.cpp
@@ -1,12 +1,18 @@
 #include "register_types.h"
 
 #include "render.h"
+#include "sim_adapter.h"
 
 void initialize_ftmodules_module(ModuleInitializationLevel p_level) {
     if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
         return;
     }
     ClassDB::register_class<FTRender>();
+
+    ClassDB::register_class<FTBlock>();
+    ClassDB::register_class<FTRect>();
+    ClassDB::register_class<FTBackend>();
+    ClassDB::register_class<FTDesign>();
 }
 
 void uninitialize_ftmodules_module(ModuleInitializationLevel p_level) {
